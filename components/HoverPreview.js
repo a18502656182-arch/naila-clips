5ebcaaca083f13175bf1183ebee01777a40a4b40
a18502656182-arch <a18502656182@gmail.com>
2026-03-03 16:27:13 +0800

    Update HoverPreview.js

--- components/HoverPreview.js
+++ components/HoverPreview.js
@@ -1,5 +1,6 @@
 // components/HoverPreview.js
 import { useEffect, useMemo, useRef, useState } from "react";
+import Image from "next/image";
 
 function isCoarsePointer() {
   if (typeof window === "undefined") return true;
@@ -61,13 +62,12 @@ export default function HoverPreview({
     >
       {/* 封面 */}
       {coverUrl ? (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img
+        <Image
           src={coverUrl}
           alt={alt}
+          fill
+          sizes="(max-width: 768px) 100vw, 400px"
           style={{
-            width: "100%",
-            height: "100%",
             objectFit: "cover",
             display: showVideo ? "none" : "block",
           }}
