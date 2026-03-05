6c8b2825381f1b1e459799967b50b81a57eaf9c0
a18502656182-arch <a18502656182@gmail.com>
2026-03-05 16:07:21 +0800

    Update JournalClient.js

--- app/journal/JournalClient.js
+++ app/journal/JournalClient.js
@@ -397,7 +397,20 @@ const POSTER_THEMES = [
   },
 ];
 
--r,y+h,r); ctx.lineTo(x+r,y+h);
+function PosterGenerator({ me, streakDays, totalVideos, vocabCount, masteredCount, heatmapData, tasks }) {
+  const canvasRef = useRef(null);
+  const [generating, setGenerating] = useState(false);
+  const [posterUrl, setPosterUrl] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [themeIdx, setThemeIdx] = useState(0);
+
+  const doneCount = tasks.filter(t => t.done).length;
+
+  function roundRect(ctx, x, y, w, h, r) {
+    ctx.beginPath();
+    ctx.moveTo(x+r,y); ctx.lineTo(x+w-r,y);
+    ctx.arcTo(x+w,y,x+w,y+r,r); ctx.lineTo(x+w,y+h-r);
+    ctx.arcTo(x+w,y+h,x+w-r,y+h,r); ctx.lineTo(x+r,y+h);
     ctx.arcTo(x,y+h,x,y+h-r,r); ctx.lineTo(x,y+r);
     ctx.arcTo(x,y,x+r,y,r); ctx.closePath();
   }
